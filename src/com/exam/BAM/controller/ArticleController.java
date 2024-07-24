a6be00492052abcbf6e1d1562fa7f98636cc8061
sooooo56 <sooooo4645@gmail.com>
2024-07-24 21:44:43 +0900

    로그인, 로그아웃 추가

--- src/com/exam/BAM/controller/ArticleController.java
+++ src/com/exam/BAM/controller/ArticleController.java
@@ -10,7 +10,7 @@ import com.exam.BAM.util.Util;
 public class ArticleController extends Controller {
 
 	private List<Article> articles;
-	
+
 	public ArticleController(Scanner sc) {
 		this.sc = sc;
 		this.lastId = 0;
@@ -20,7 +20,7 @@ public class ArticleController extends Controller {
 	@Override
 	public void doAction(String cmd, String methodName) {
 		this.cmd = cmd;
-		
+
 		switch (methodName) {
 		case "write":
 			doWrite();
@@ -41,85 +41,98 @@ public class ArticleController extends Controller {
 			System.out.println("존재하지 않는 명령어 입니다");
 		}
 	}
-	
+
 	private void doWrite() {
+
+		if (loginedMember == null) {
+			System.out.println("로그인이 되어 있지 않습니다");
+			return;
+		}
+		
 		System.out.printf("제목 : ");
 		String title = sc.nextLine();
 		System.out.printf("내용 : ");
 		String body = sc.nextLine();
-		
+
 		lastId++;
-        
+
 		Article article = new Article(lastId, Util.getDateStr(), title, body, 0);
-		
+
 		articles.add(article);
-		
+
 		System.out.println(lastId + "번 글이 생성되었습니다");
+
 	}
-	
+
 	private void showList() {
 		if (articles.size() == 0) {
 			System.out.println("게시글이 없습니다");
 			return;
 		}
-		
+
 		List<Article> printArticles = articles;
-		
+
 		String searchKeyword = cmd.substring("article list".length()).trim();
-		
+
 		if (searchKeyword.length() > 0) {
 			System.out.println("검색어 : " + searchKeyword);
 
 			printArticles = new ArrayList<>();
-			
+
 			for (Article article : articles) {
 				if (article.getTitle().contains(searchKeyword)) {
 					printArticles.add(article);
 				}
 			}
-			
+
 			if (printArticles.size() == 0) {
 				System.out.println("검색결과가 없습니다");
 				return;
 			}
 		}
-		
+
 		System.out.println("번호	|	제목	|		작성일		|	조회수");
-		
+
 		for (int i = printArticles.size() - 1; i >= 0; i--) {
 			Article article = printArticles.get(i);
-			System.out.printf("%d	|	%s	|	%s	|	%d\n", article.getId(), article.getTitle(), article.getRegDate(), article.getViewCnt());
+			System.out.printf("%d	|	%s	|	%s	|	%d\n", article.getId(), article.getTitle(),
+					article.getRegDate(), article.getViewCnt());
 		}
 	}
 
 	private void showDetail() {
 		int id = getIdByCmd(cmd);
-		
+
 		if (id == 0) {
 			System.out.println("명령어가 올바르지 않습니다");
 			return;
 		}
-		
+
 		Article foundArticle = getArticleById(id);
-		
+
 		if (foundArticle == null) {
 			System.out.println(id + "번 게시물은 존재하지 않습니다");
 			return;
 		}
-		
+
 		foundArticle.increaseViewCnt();
-		
+
 		System.out.printf("번호 : %d\n", foundArticle.getId());
 		System.out.printf("작성일 : %s\n", foundArticle.getRegDate());
 		System.out.printf("제목 : %s\n", foundArticle.getTitle());
 		System.out.printf("내용 : %s\n", foundArticle.getBody());
 		System.out.printf("조회수 : %d\n", foundArticle.getViewCnt());
-		
+
 	}
 
 	private void doModify() {
-		int id = getIdByCmd(cmd);
+		if (loginedMember == null) {
+			System.out.println("로그인이 되어 있지 않습니다");
+			return;
+		}
 		
+		int id = getIdByCmd(cmd);
+
 		if (id == 0) {
 			System.out.println("명령어가 올바르지 않습니다");
 			return;
@@ -144,28 +157,32 @@ public class ArticleController extends Controller {
 	}
 
 	private void doDelete() {
+		if (loginedMember == null) {
+			System.out.println("로그인이 되어 있지 않습니다");
+			return;
+		}
 		int id = getIdByCmd(cmd);
-		
+
 		if (id == 0) {
 			System.out.println("명령어가 올바르지 않습니다");
 			return;
 		}
-		
+
 		Article foundArticle = getArticleById(id);
-		
+
 		if (foundArticle == null) {
 			System.out.println(id + "번 게시물은 존재하지 않습니다");
 			return;
 		}
-		
+
 		articles.remove(foundArticle);
-		
+
 		System.out.println(id + "번 게시물을 삭제했습니다");
 	}
-	
+
 	private int getIdByCmd(String cmd) {
 		String[] cmdBits = cmd.split(" ");
-		
+
 		try {
 			int id = Integer.parseInt(cmdBits[2]);
 			return id;
@@ -177,19 +194,19 @@ public class ArticleController extends Controller {
 	}
 
 	private Article getArticleById(int id) {
-		for (Article article : articles){
+		for (Article article : articles) {
 			if (id == article.getId()) {
 				return article;
 			}
 		}
 		return null;
 	}
-	
+
 	@Override
 	public void makeTestData() {
 		System.out.println("테스트용 게시물 데이터 3개를 생성했습니다");
 		for (int i = 1; i <= 3; i++) {
 			articles.add(new Article(++lastId, Util.getDateStr(), "제목" + i, "내용" + i, i * 10));
-		}		
+		}
 	}
 }
--- src/com/exam/BAM/controller/MemberController.java
+++ src/com/exam/BAM/controller/MemberController.java
@@ -10,11 +10,13 @@ import com.exam.BAM.util.Util;
 public class MemberController extends Controller {
 
 	private List<Member> members;
+//	private Member loginedMember;
 	
 	public MemberController(Scanner sc) {
 		this.sc = sc;
 		this.lastId = 0;
 		this.members = new ArrayList<>();
+		loginedMember = null;
 	}
 	
 	@Override
@@ -23,11 +25,17 @@ public class MemberController extends Controller {
 		case "join":
 			doJoin();
 			break;
+		case "login":
+			doLogin();
+			break;
+		case "logout":
+			doLogout();
+			break;
 		default:
 			System.out.println("존재하지 않는 명령어 입니다");
 		}
 	}
-	
+
 	private void doJoin() {
 		String loginId = null;
 		String loginPw = null;
@@ -42,16 +50,9 @@ public class MemberController extends Controller {
 				continue;
 			}
 			
-			boolean isLoginIdDup = false;
+			Member member = getMemberByLoginId(loginId);
 			
-			for (Member member : members) {
-				if (loginId.equals(member.getLoginId())) {
-					isLoginIdDup = true;
-					break;
-				}
-			}
-			
-			if (isLoginIdDup) {
+			if (member != null) {
 				System.out.printf("[ %s ]은(는) 이미 사용중인 아이디입니다\n", loginId);
 				continue;
 			}
@@ -98,7 +99,64 @@ public class MemberController extends Controller {
 		
 		System.out.println(name + "님이 가입되었습니다");
 	}
+	
+	private void doLogin() {
+		if (loginedMember != null) {
+			System.out.println("이미 로그인이 되어있습니다");
+			return;
+		}
+		
+		System.out.printf("아이디 : ");
+		String loginId = sc.nextLine().trim();
+		System.out.printf("비밀번호 : ");
+		String loginPw = sc.nextLine().trim();
+		
+		if (loginId.length() == 0) {
+			System.out.println("아이디를 입력해주세요");
+			return;
+		}
+		
+		if (loginPw.length() == 0) {
+			System.out.println("비밀번호를 입력해주세요");
+			return;
+		}
+		
+		Member member = getMemberByLoginId(loginId);
+		
+		if (member == null) {
+			System.out.printf("[ %s ]은(는) 존재하지 않는 아이디입니다\n", loginId);
+			return;
+		}
+		
+		if (member.getLoginPw().equals(loginPw) == false) {
+			System.out.println("비밀번호를 확인해주세요");
+			return;
+		}
+		
+		loginedMember = member;
+		
+		System.out.printf("[ %s ] 회원님 환영합니다~\n", member.getName());
+	}
+	
+	private void doLogout() {
+		if (loginedMember == null) {
+			System.out.println("로그인이 되어 있지 않습니다");
+			return;
+		}
+		
+		loginedMember = null;
+		System.out.println("정상적으로 로그아웃 되었습니다");
+	}
 
+	private Member getMemberByLoginId(String loginId) {
+		for (Member member : members) {
+			if (member.getLoginId().equals(loginId)) {
+				return member;
+			}
+		}
+		return null;
+	}
+	
 	@Override
 	public void makeTestData() {
 		System.out.println("테스트용 회원 데이터 3개를 생성했습니다");
